63f491e39d2c4b26ca55fb8d7880d02d68409abe
Ruben Paz <me@ruben.io>
2018-09-15 15:29:54 +0100

    Basic commands and REPL help

--- src/bin/repl.rs
+++ src/bin/repl.rs
@@ -2,23 +2,85 @@ use console::Style;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 use simpledb::SimpleDbConfig;
+use std::fmt;
+use std::process;
+
+struct REPLCommand<'a> {
+    pub cmd: &'a str,
+    pub help: &'a str,
+    pub is_match: &'a Fn(&str) -> bool,
+    pub execute: &'a Fn((&SimpleDbConfig, &str)) -> (),
+}
+
+impl<'a> fmt::Display for REPLCommand<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{cmd:<width$}{help}",
+            cmd = self.cmd,
+            width = 10,
+            help = self.help
+        )
+    }
+}
+
+fn print_help() -> () {
+    let result: String = COMMANDS
+        .iter()
+        .map(|command| format!("\t{}", command))
+        .fold(String::new(), |acc, next| format!("{}\n{}", acc, next));
+    println!("\nAvailable commands:  {}\n", result)
+}
+
+const COMMANDS: [REPLCommand; 3] = [
+    REPLCommand {
+        cmd: "[SQL]",
+        help: "Executes a query agains the db",
+        is_match: &|_| false,
+        execute: &|_| panic!("Not yet implemented"),
+    },
+    REPLCommand {
+        cmd: "\\help",
+        help: "Prints this help page",
+        is_match: &|candidate| candidate == "\\help",
+        execute: &|_| {
+            print_help();
+        },
+    },
+    REPLCommand {
+        cmd: "\\q",
+        help: "Exits the REPL",
+        is_match: &|candidate| candidate == "\\q",
+        execute: &|_| {
+            process::exit(0);
+        },
+    },
+];
+
+fn handle_meta_commands(db_config: &SimpleDbConfig, candidate: &str) -> () {
+    for command in COMMANDS.iter() {
+        let is_match = command.is_match;
+        if is_match(candidate) {
+            let execute = command.execute;
+            return execute((db_config, candidate));
+        }
+    }
+
+    let error_text = Style::new().red().bold().apply_to("Error:");
+    println!("{} Command \"{}\" not recognized", error_text, candidate);
+    print_help();
+}
 
 pub fn start(db_config: SimpleDbConfig) {
     let mut rl = Editor::<()>::new();
     let style_bold = Style::new().bold();
-    let style_error = Style::new().red().bold();
-    let error_text = style_error.apply_to("Error:");
     loop {
-        let readline = rl.readline(&format!("({})> ", style_bold.apply_to(db_config.db_name)));
+        let readline = rl.readline(&format!("({})> ", style_bold.apply_to(&db_config.db_name)));
 
         match readline {
             Ok(line) => {
-                if line.chars().next().unwrap() == '\\' {
-                    rl.add_history_entry(line.clone());
-                    println!("{} {}", error_text, "Command not found")
-                } else {
-
-                }
+                rl.add_history_entry(line.clone());
+                handle_meta_commands(&db_config, &line);
             }
             Err(ReadlineError::Interrupted) => break,
             Err(ReadlineError::Eof) => break,
